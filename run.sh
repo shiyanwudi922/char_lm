66b2ba24ff1289f0a2f079f6b5e8d8fd3ce805a7
ocean <ocean@oceandeMacBook-Pro.local>
2017-12-05 11:30:51 +0800

    add readme

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ python run.py \
 --num_layers 3 \
 --use_embedding \
 --use_sample_loss \
---set_learning_rate 0.01 \
+--learning_rate 0.01 \
 --learning_rate_decay_factor 0.5 \
---input_file /Users/ocean/projects/char_lm/data/poetry.txt \
+--input_file /Users/baihai/projects/char_lm/data/The_Return_of_the_Condor_Heroes.txt \
 --max_train_steps 10000 \
 --steps_per_sentence_length 1000 \
 --steps_per_checkpoint 500 \
